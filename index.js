a248d238beff9553c9dd35f636bdccb8af64eaa0
Sung Won Cho <mikeswcho@gmail.com>
2016-02-16 15:24:40 +1100

    Allow only one search to be running at a time

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
+var intervalIds = [];
+
+function flush() {
+  intervalIds.forEach(function (iid) {
+    window.clearInterval(iid);
+  });
+}
+
 chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
   if (request.action === 'searchAndHighlight') {
-    $(document.body).removeHighlight();
-    $(document.body).highlight(request.searchString);
-    $('.highlight').css('background-color', request.color);
+    var searchString = request.searchString;
+    var color = request.color;
+
+    var run = function (cb) {
+      $(document.body).removeHighlight();
+      $(document.body).highlight(searchString);
+      $('.highlight').css('background-color', color);
+    };
+
+    if (intervalIds.length) {
+      flush();
+    }
+
+    var intervalId = window.setInterval(function () {
+      run();
+    }, 1000);
+
+    intervalIds.push(intervalId);
 
     sendResponse({done: true});
   }
